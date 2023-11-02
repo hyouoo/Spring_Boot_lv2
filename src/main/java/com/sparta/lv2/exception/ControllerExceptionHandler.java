7600922ec51fd7dfe56eb204722b42a4143133fe
hyouoo <hyouoo@gmail.com>
2023-11-02 15:50:33 +0900

    ExceptionHandler modify

--- src/main/java/com/sparta/lv2/exception/ControllerExceptionHandler.java
+++ src/main/java/com/sparta/lv2/exception/ControllerExceptionHandler.java
@@ -4,9 +4,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ControllerExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
